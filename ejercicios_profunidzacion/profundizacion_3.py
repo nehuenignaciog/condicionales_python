41e5c4c956552e1c7d3cca586b1e140a9c62e311
nehuenignaciog <nehuenignaciog@gmail.com>
2022-03-26 19:46:47 -0300

    Pendiente de revisión

--- ejercicios_profunidzacion/profundizacion_3.py
+++ ejercicios_profunidzacion/profundizacion_3.py
@@ -30,3 +30,37 @@ contenido. Recomendamos pensar bien este problema de lógica con un lápiz y pap
 
 print('Ejercicios de práctica con números')
 # Empezar aquí la resolución del ejercicio
+
+temperatura_1 = int(input("Ingrese la  primer temperatura:\n"))
+
+temperatura_2 = int(input("Ingrese la Segunda temperatura:\n"))
+
+temperatura_3 = int(input("Ingrese la Tercer temperatura:\n"))
+
+
+# Imprimo la temperatura máxima
+
+if (temperatura_2 < temperatura_1 and temperatura_3 < temperatura_1):
+    print ("La primera temperatura es la máxima ({})".format(temperatura_1))
+elif (temperatura_1 < temperatura_2 and temperatura_3 < temperatura_2):
+    print ("La segunda temperatura es la máxima ({})".format(temperatura_2))
+else:
+    print ("La tercer temperatura es la máxima ({})".format(temperatura_3))
+
+# Imprimo la temperatura mínima
+
+if (temperatura_2 > temperatura_1 and temperatura_3 > temperatura_1):
+    print ("La primera temperatura es la máxima ({})".format(temperatura_1))
+elif (temperatura_1 > temperatura_2 and temperatura_3 > temperatura_2):
+    print ("La segunda temperatura es la máxima ({})".format(temperatura_2))
+else:
+    print ("La tercer temperatura es la mínima ({})".format(temperatura_3))
+
+# Calculo el promedio 
+
+suma = temperatura_1 + temperatura_2 + temperatura_3
+
+promedio = suma / 3
+
+# Imprimo promedio
+print ("El promedio es: {}".format(promedio))
