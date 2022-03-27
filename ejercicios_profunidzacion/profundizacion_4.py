235537f428e9926e4019e081aadb17c7060ceaed
nehuenignaciog <nehuenignaciog@gmail.com>
2022-03-26 21:23:49 -0300

    Pendiente de revisión

--- ejercicios_profunidzacion/profundizacion_4.py
+++ ejercicios_profunidzacion/profundizacion_4.py
@@ -4,7 +4,7 @@
 # Autor: Inove Coding School
 # Version: 2.0
 
-# NOTA: 
+# NOTA:
 # Estos ejercicios son de mayor dificultad que los de clase y práctica.
 # Están pensados para aquellos con conocimientos previo o que dispongan
 # de mucho más tiempo para abordar estos temas por su cuenta.
@@ -33,3 +33,103 @@ contenido.
 
 print('Ejercicios de práctica con cadenas')
 # Empezar aquí la resolución del ejercicio
+
+# Declaro funciones de ordenamiento
+
+def ordenar_alfabetico(valor_1,valor_2,valor_3):
+   
+    if (valor_2 < valor_1 and valor_3 < valor_1):
+        orden_1 = valor_1
+        if (valor_2 < valor_3):
+            orden_3 = valor_2
+            orden_2 = valor_3
+        else:
+            orden_3 = valor_3
+            orden_2 = valor_2
+    elif (valor_1 < valor_2 and valor_3 < valor_2):
+        orden_1 = valor_2
+        if (valor_1 < valor_3):
+            orden_3 = valor_1
+            orden_2 = valor_3
+        else:
+            orden_3 = valor_3
+            orden_2 = valor_1
+    else:
+        orden_1 = valor_3
+        if (valor_1 < valor_2):
+            orden_3 = valor_1
+            orden_2 = valor_2
+        else:
+            orden_3 = valor_2
+            orden_2 = valor_1
+ 
+    print("\nOrden Alfabetico\n")
+    print ("1 - {}\n".format(orden_3))
+    print ("2 - {}\n".format(orden_2))
+    print ("3 - {}\n".format(orden_1))
+   
+def ordernar_cantidad_letras(valor_1, valor_2, valor_3):
+   len_valor_1= len(valor_1)
+   len_valor_2= len(valor_2)
+   len_valor_3= len(valor_3)
+   if (len_valor_2 < len_valor_1 and len_valor_3 < len_valor_1):
+        orden_1 = valor_1
+        if (len_valor_2 < len_valor_3):
+            orden_3 = valor_2
+            orden_2 = valor_3
+        else:
+            orden_3 = valor_3
+            orden_2 = valor_2
+   elif (len_valor_1 < len_valor_2 and len_valor_3 < len_valor_2):
+        orden_1 = valor_2
+        if (len_valor_1 < len_valor_3):
+            orden_3 = valor_1
+            orden_2 = valor_3
+        else:
+            orden_3 = valor_3
+            orden_2 = valor_1
+   else:
+        orden_1 = valor_3
+        if (len_valor_1 < len_valor_2):
+            orden_3 = valor_1
+            orden_2 = valor_2
+        else:
+            orden_3 = valor_2
+            orden_2 = valor_1
+ 
+   print("\nOrden por cantidad de letras (mayor a menor) \n")
+   print ("1 - {}\n".format(orden_1))
+   print ("2 - {}\n".format(orden_2))
+   print ("3 - {}\n".format(orden_3))
+
+# Solicito Ingreso de palabras
+
+print ("Por favor ingrese tres palabras cualesquiera.")
+
+texto_1 = str(input('Ingrese la primera:\n')).lower()
+
+texto_2 = str(input('Ingrese la segunda:\n')).lower()
+
+texto_3 = str(input('Ingrese la tercera:\n')).lower()
+
+# Imprimo menú de opciones
+
+print ("\nPor favor seleccione el ordenamiento.\n")
+print ("1 - Ordenar por orden alfabético\n" )
+print ("2 - Ordenar por cantidad de letras\n")
+opcion = str(input())
+
+
+
+# Imprimo resultado
+
+if (opcion == '1'):
+    ordenar_alfabetico (texto_1,texto_2,texto_3)
+
+elif (opcion == '2'):
+    
+     ordernar_cantidad_letras(texto_1,texto_2,texto_3)
+    
+else:
+    print ("Seleccion incorrecta. Por favor vuelva a ejecutar el programa.")
+
